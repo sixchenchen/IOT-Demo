b8264e5c5cb314ffbf99e0ff24d3d4329fd0f6e4
陈文杰 <1637074001@qq.com>
2025-10-03 11:37:19 +0800

    用户新增业务代码

---
+++ common/src/main/java/com/moyan/constant/TokenConstant.java
@@ -0,0 +1,17 @@
+package com.moyan.constant;
+
+/**
+ * @program: IOT-Demo
+ * @ClassName TokenConstant
+ * @description:
+ * @author: chen
+ * @create: 2025-10-03 11:19
+ **/
+
+public class TokenConstant {
+    public static final String TOKEN_HEADER = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String TOKEN_KEY = "token";
+
+    public static final String TOKEN_INFO = "login:";
+}
